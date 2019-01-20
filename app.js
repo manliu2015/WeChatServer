20433cdcafbf131f3f0773b3706c4a26037b08e0
System Administrator <root@liuhongbindeMacBook-Pro.local>
2019-01-20 16:56:21 +0800

    init

---
+++ app.js
@@ -0,0 +1,59 @@
+// app.js
+// Create by manliu Nov/24/2018
+// app 入口函数
+
+// app初始化
+var express = require('express')
+
+var app = express()
+
+// 设置服务器标示
+app.serverType = 'wcs'
+app.serverName = 'wechat_server'
+
+// 放到全局变量中
+global.app = app
+
+// 初始化日志模块
+
+// 初始化服务器
+app.start_server = function(succ_cb) {
+    try {
+        app.sysLog('start init server ...')
+
+        // 模块管理器
+        var model = require('./src/ServerManager').GetModel
+        app.ServerManager = model
+        var ServerManager = model()
+
+        // 初始化服务器
+        ServerManager.start(function(errorMsg) {
+            if (errorMsg) {
+                app.errLog('server init fail, %s', errorMsg)
+                return
+            }
+        })
+    } catch (e) {
+        console.error(e.name, e.message, e.stack)
+        return
+    }
+
+    app.sysLog('server(%s) init done.', app.serverName)
+
+    // 初始化成功，执行回调
+    if (typeof succ_cb === 'function') {
+        succ_cb()
+    }
+}
+
+// 监听异常
+process.on('uncaughtException', function(error) {
+    // app.errLog(
+    //     'global error (%s):(%s)\n(%s)',
+    //     error.name,
+    //     error.message,
+    //     error.stack
+    // )
+})
+
+module.exports = app
